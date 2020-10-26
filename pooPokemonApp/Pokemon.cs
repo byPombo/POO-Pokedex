ac587cfadb90821658a136eaf1de8d1ec87f6e33
Daniel Victor <daniel.silva@apinteractive.com.br>
2020-10-26 10:20:04 -0300

    Lista nome/descrição - Construção de classes

---
+++ pooPokemonApp/Pokemon.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pooPokemonApp
+{
+    public class Pokemon
+    {
+        public Pokemon()
+        {
+            this.Nome = "";
+            this.Descricao = "";
+        }
+        private string nome;
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+            set
+            {
+                string texto = value;
+                nome = texto;
+            }
+        }
+
+        private string descricao; //Armazena o valor da propriedade Descrição
+
+        public string Descricao // Representa a característica Descrição do Pokemon
+        {
+            get { return this.descricao; }
+            set { this.descricao = value; }
+        }
+
+        public void ExibirDadosPokemon()
+        {
+            Console.WriteLine("Nome do Pokémon: " + this.Nome);
+            Console.WriteLine("Descrição do Pokémon: " + this.Descricao);
+        }
+    }
+}
---
+++ pooPokemonApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace pooPokemonApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pokemon p = new Pokemon();
+            p.Nome = "Bulbasauro";
+            p.Descricao = ("Bulbasauro é um pokemon tipo planda...");
+            p.ExibirDadosPokemon();
+            Console.ReadKey();
+        }
+    }
+}
