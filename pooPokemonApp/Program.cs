f995867bf41e8203ac8fcbe0ec830718ecc2a6ca
Daniel Victor <daniel.silva@apinteractive.com.br>
2020-10-26 13:26:49 -0300

    Implementar uma Pokédex - Revisão de conceitos pt.2

--- pooPokemonApp/Program.cs
+++ pooPokemonApp/Program.cs
@@ -7,8 +7,27 @@ namespace pooPokemonApp
         static void Main(string[] args)
         {
             Pokedex pokedex = new Pokedex();
-            pokedex.ListarPokemons();
-            Console.ReadKey();
+            int resp = 100;
+            while (resp != 0)
+            {
+                resp = menu();
+                if (resp == 1)
+                {
+                    pokedex.ListarPokemons();
+                }
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        static int menu()
+        {
+            Console.WriteLine("Pokédex ---- Agenda Pokémon");
+            Console.WriteLine("0 - SAir da Pokédex");
+            Console.WriteLine("1 - Listar todos os Pokémons");
+            Console.WriteLine("Opção: ");
+            int resp = Convert.ToInt32(Console.ReadLine());
+            return resp;
         }
     }
 }
